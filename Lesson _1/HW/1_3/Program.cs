2044d30ad16f463e597e815ae6f5f8d0e8905584
AleksBu <yakut0385@gmail.com>
2023-02-25 16:55:23 +0300

    HW4

---
+++ Lesson _1/HW/1_3/Program.cs
@@ -0,0 +1,11 @@
+﻿string s_a = Console.ReadLine()!;
+
+int a = int.Parse(s_a);
+
+if (a%2 == 0)
+{
+    Console.WriteLine("Четное");
+}
+
+else
+Console.WriteLine("Нечетное");
---
+++ Lesson _1/HW/1_4/Program.cs
@@ -0,0 +1,11 @@
+﻿string s_a = Console.ReadLine()!;
+
+int a = int.Parse(s_a);
+
+int b = 2;
+
+while(b <= a)
+{
+    Console.Write(b+" ");
+    b = b + 2;
+}
