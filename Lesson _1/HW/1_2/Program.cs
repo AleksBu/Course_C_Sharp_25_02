8f4cd2ba4c3e29011e4fff3184fac4371e1e0c79
AleksBu <yakut0385@gmail.com>
2023-02-25 16:13:10 +0300

    hw2

---
+++ Lesson _1/HW/1_2/Program.cs
@@ -0,0 +1,20 @@
+﻿string s_a = Console.ReadLine()!;
+string s_b = Console.ReadLine()!;
+string s_c = Console.ReadLine()!;
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+int c = int.Parse(s_c);
+int max = a;
+
+if (a < b)
+{
+    max = b;  
+}
+
+if (b < c)
+{
+    max = c;
+}
+
+Console.WriteLine(max);
