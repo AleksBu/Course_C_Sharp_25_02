26a6d22253253cdc5f3f602b71f1de97ff3086c5
AleksBu <yakut0385@gmail.com>
2023-02-25 15:45:16 +0300

    hw1

---
+++ Lesson _1/HW/1_1/Program.cs
@@ -0,0 +1,24 @@
+﻿string s_a = Console.ReadLine()!;
+string s_b = Console.ReadLine()!;
+
+int a = int.Parse(s_a);
+int b = int.Parse(s_b);
+int max = 0;
+
+if (a > b)
+{
+    max = a;
+    Console.WriteLine(max);
+}
+
+else if (a < b)
+{
+    max = b;
+    Console.WriteLine(max);
+}
+
+else
+{
+    Console.WriteLine("the numbers are equal");
+}
+
