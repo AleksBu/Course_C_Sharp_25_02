03d4a1a49c8ce556a04c16684ac67d7e5e597051
AleksBu <yakut0385@gmail.com>
2023-02-26 22:23:19 +0300

    hw2_4

---
+++ Lesson_2/HW/2_4/Program.cs
@@ -0,0 +1,35 @@
+﻿string s_a = Console.ReadLine()!;
+
+if (s_a == "1")
+{
+    Console.WriteLine("Monday. Go to work");
+}
+else if (s_a == "2")
+{
+    Console.WriteLine("Tuesday. Go to work");
+}
+else if (s_a == "3")
+{
+    Console.WriteLine("Wednesday. Go to work");
+}
+else if (s_a == "4")
+{
+    Console.WriteLine("Thursday. Go to work");
+}
+else if (s_a == "5")
+{
+    Console.WriteLine("Friday. Go to work");
+}
+else if (s_a == "6")
+{
+    Console.WriteLine("Saturday! Let's have a rest");
+}
+else if (s_a == "7")
+{
+    Console.WriteLine("Sunday! Let's have a rest");
+}
+
+else 
+{
+    Console.WriteLine("It's not weekday number");
+}
