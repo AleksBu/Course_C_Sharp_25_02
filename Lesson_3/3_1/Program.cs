ccae2033309008218ea64a82e1259f6b02fc807f
AleksBu <yakut0385@gmail.com>
2023-03-04 21:25:54 +0300

    3_1

--- Lesson_3/3_1/Program.cs
+++ Lesson_3/3_1/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿void Quarters(int num )
+{
+    if (num == 1)
+        Console.WriteLine("x > 0, y > 0");
+    else if (num == 2)
+        Console.WriteLine("x < 0, y > 0");
+    else if (num == 3)
+        Console.WriteLine("x < 0, y < 0");
+    else if (num == 4)
+        Console.WriteLine("x > 0, y < 0");
+    else Console.WriteLine("Error");
+}
+int num_1 = int.Parse(Console.ReadLine()!);
+Quarters(num_1);
