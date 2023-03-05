ffe1c664db2150a7cbb2766eb24860240daa3ad0
AleksBu <yakut0385@gmail.com>
2023-03-05 22:10:10 +0300

    HW3_2

---
+++ Lesson_3/HW/3_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+void Cube(int num)
+{
+    for(int i = 1; i <=num; i++)
+    {
+        Console.Write(Math.Pow(i, 3) + ", ");
+    }
+
+}
+int x = int.Parse(Console.ReadLine()!);
+Cube(x);
