2f66e99bdedc4b66e10d64c2120f2ad6c9c6a952
AleksBu <yakut0385@gmail.com>
2023-03-05 22:50:32 +0300

    HW 4_0

---
+++ Lesson_4/HW/4_0/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+
+int SumNum(int A, int B)
+{
+    int Degree = A;
+    for (int i = 1; i < B; i++)
+    {
+        Degree = Degree * A;
+    }
+    return Degree;
+}
+
+int A = int.Parse(Console.ReadLine()!);
+int B = int.Parse(Console.ReadLine()!);
+int result = SumNum(A, B);
+Console.WriteLine(result);
