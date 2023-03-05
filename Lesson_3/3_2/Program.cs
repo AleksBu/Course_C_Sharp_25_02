968a3a343bac2f702bd07cdebb86a8fa6158419e
AleksBu <yakut0385@gmail.com>
2023-03-05 22:21:35 +0300

    finish hw3

--- Lesson_3/3_2/Program.cs
+++ Lesson_3/3_2/Program.cs
@@ -1,4 +1,7 @@
-﻿double example (double x1, double y1, double x2, double y2)
+﻿// // 2. Напишите программу, которая принимает на вход координаты
+// двух точек и находит расстояние между ними в 2D пространстве.
+
+double example (double x1, double y1, double x2, double y2)
 {
     return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 }
