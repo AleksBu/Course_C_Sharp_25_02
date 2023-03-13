bb55b8fe0a004b685bb18a4d9acc36551d3f78b8
AleksBu <yakut0385@gmail.com>
2023-03-13 22:53:11 +0400

    6_extra

--- Lesson_6/HW/6_ext/Program.cs
+++ Lesson_6/HW/6_ext/Program.cs
@@ -1,37 +1,52 @@
 ﻿// Напишите программу, которая будет создавать копию
 // заданного массива с помощью поэлементного копирования.
 
-void PrdoubleArray(double[,]origin)
+void PrintArray(double[,] origin)
 {
-    for (double i = 0; i < origin.GetLength(0); i++)
+    for (int i = 0; i < origin.GetLength(0); i++)
     {
-        for (double j = 0; j < origin.GetLength(1); j++)
+        for (int j = 0; j < origin.GetLength(1); j++)
         {
-           Console.Write($"{origin[i, j]} ");
+            Console.Write($"{string.Format("{0:F2}", origin[i, j])} ");
         }
-    Console.WriteLine();
+        Console.WriteLine();
     }
+    Console.WriteLine();
 }
 
 void FillArray(double[,] origin)
 {
-    for (double i = 0; i < origin.GetLength(0); i++)
+    for (int i = 0; i < origin.GetLength(0); i++)
     {
-        for (double j = 0; j < origin.GetLength(1); j++)
+        for (int j = 0; j < origin.GetLength(1); j++)
         {
-            origin[i, j] = new Random().Next(1,10);
+            Random rnd = new Random();
+            origin[i, j] = rnd.NextDouble() * (9.9 + 0.1) - 0.1;
         }
     }
 }
 
-// void Copy(double[,] origin)
-// {
-//     for
-// }
+void CopyPast(double[,] origin, double[,] copy)
+{   
+    for (int i = 0; i < origin.GetLength(0); i++)
+    {
+        for (int j = 0; j < copy.GetLength(1); j++)
+        {
+            copy[i, j] = origin[i, j];
+            Console.Write($"{string.Format("{0:F2}", copy[i, j])} ");
+        }
+        Console.WriteLine();
+    }
+}
 
-double row = double.Parse(Console.ReadLine()!);
-double colm = double.Parse(Console.ReadLine()!);
-double[,] origin = new double [row,colm];
+int row = int.Parse(Console.ReadLine()!);
+int colm = int.Parse(Console.ReadLine()!);
+double[,] origin = new double[row, colm];
+double[,] copy = new double[row, colm];
 
 FillArray(origin);
-PrdoubleArray(origin);
+PrintArray(origin);
+CopyPast(origin, copy);
+
+
+
