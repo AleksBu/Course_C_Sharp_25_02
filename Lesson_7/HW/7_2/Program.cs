e14cee92d1bdc31afacfff482b062e3229b192c9
AleksBu <yakut0385@gmail.com>
2023-03-19 14:57:12 +0400

    HW7_2

---
+++ Lesson_7/HW/7_2/Program.cs
@@ -0,0 +1,49 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+void Print(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+void Coordinate(int [,] arr, int posRow, int posColumn)
+{
+    if ((posRow <= arr.GetLength(0)) || (posColumn <= arr.GetLength(1)))
+    {
+        Console.WriteLine($" {arr[posRow - 1, posColumn - 1]} ");
+    }
+    else
+    {
+        Console.WriteLine("Coordinate is out side of the matrix ");
+    }
+}
+int num_row = int.Parse(Console.ReadLine()!);
+int num_column = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(num_row, num_column, start, stop);
+Print(mass);
+
+int posRow = int.Parse(Console.ReadLine()!);
+int posColumn = int.Parse(Console.ReadLine()!);
+Coordinate(mass, posRow, posColumn);
