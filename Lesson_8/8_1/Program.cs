a597f99e0390528c6e178e64c64542d238ec42f8
AleksBu <yakut0385@gmail.com>
2023-03-19 19:17:12 +0400

    8_1

--- Lesson_8/8_1/Program.cs
+++ Lesson_8/8_1/Program.cs
@@ -26,84 +26,34 @@ int[,] MassNums(int row, int column, int from, int to)
     return arr;
 }
 
-void FirstWithLast(int[,] arr)
+void ReplaceColumnRows(int[,] arr)
 {
     int row = arr.GetLength(0);
     int column = arr.GetLength(1);
 
-    for (int j = 0; j < column; j++)
-        (arr[0, j], arr[row - 1, j]) = (arr[row - 1, j], arr[0, j]);
+    if (row != column)
+    {
+      Console.WriteLine("Замена невозможна");
+      return;
+    }
+    for (int i = 0; i < row; i++)
+      for (int j = 0; j < i; j++)
+        (arr[i, j], arr[j, i]) = (arr[j, i], arr[i, j]);
 }
+
 
 Console.Write("Enter the number of rows: ");
 int row_num = int.Parse(Console.ReadLine()!);
 Console.Write("Enter the number of columns: ");
 int column_num = int.Parse(Console.ReadLine()!);
 
+Console.Write("Enter the min number of massive ");
 int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the max number of massive ");
 int stop = int.Parse(Console.ReadLine()!);
 
 int[,] mass = MassNums(row_num, column_num, start, stop);
 Print(mass);
-FirstWithLast(mass);
-Print(mass);
-
-// Задайте двумерный массив. Напишите программу, 
-// которая заменяет строки на столбцы. В случае, если это невозможно,
-// программа должна вывести сообщение для пользователя.
-
-void Print(int[,] arr)
-{
-  int row = arr.GetLength(0);
-  int column = arr.GetLength(1);
-
-  for (int i = 0; i < row; i++)
-  {
-    for (int j = 0; j < column; j++)
-      Console.Write($" {arr[i, j]} ");
-    Console.WriteLine();
-  }
-  Console.WriteLine();
-}
-
-int[,] MassNums(int row, int column, int from, int to)
-{
-  int[,] arr = new int[row, column];
-
-  for (int i = 0; i < row; i++)
-    for (int j = 0; j < column; j++)
-      arr[i, j] = new Random().Next(from, to);
-
-  return arr;
-}
-
-void ReplaceColumnRows(int[,] arr)
-{
-  int row = arr.GetLength(0);
-  int column = arr.GetLength(1);
-
-  if (row != column)
-  {
-    Console.WriteLine("Замена невозможна");
-    return;
-  }
-  for (int i = 0; i < row; i++)
-    for (int j = 0; j < i; j++)
-      (arr[i, j], arr[j, i]) = (arr[j, i], arr[i, j]);
-}
-
-
-Console.WriteLine("Введите количество строк массива");
-int num_row = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Введите количество колонок массива");
-int num_column = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Задайте минимальное число в массиве");
-int start = int.Parse(Console.ReadLine()!);
-Console.WriteLine("Задайте максмальное число в массиве");
-int stop = int.Parse(Console.ReadLine()!);
-
-
-int[,] mass = MassNums(num_row, num_column, start, stop);
-Print(mass);
 ReplaceColumnRows(mass);
 Print(mass);
+
