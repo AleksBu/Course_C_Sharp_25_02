80be17570742c8031ae55734c6012ed4c62a2f41
AleksBu <yakut0385@gmail.com>
2023-03-25 11:43:38 +0400

    hw9_0

---
+++ Lesson_9/9_0/Program.cs
@@ -0,0 +1,23 @@
+﻿
+void ValueSet(int N)
+{
+    if (N == 0) return;
+    ValueSet(N - 1);
+    Console.Write($"{N} ");
+}
+
+int num = int.Parse(Console.ReadLine()!);
+ValueSet(num);
+
+// // 1. Задайте значение N. Напишите программу, которая выведет
+// //    все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+
+// void ValuesSet(int N)
+// {
+//     if (N == 0) return;
+//     ValuesSet(N - 1);
+//     Console.Write($"{N} ");
+// }
+
+// int num = int.Parse(Console.ReadLine()!);
+// ValuesSet(num);
