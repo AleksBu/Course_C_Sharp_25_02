15fa605ec7ae28cadf5525cdb0bd393e42d079e8
AleksBu <yakut0385@gmail.com>
2023-03-26 17:22:59 +0400

    hw9_2

---
+++ Lesson_9/HW/9_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+int ValuesSet(int M, int N)
+{
+    int sum = N;
+    if (N < M) return 0;
+    return ValuesSet(M , N - 1) + sum;
+    
+}
+
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine(ValuesSet(num1, num2));
